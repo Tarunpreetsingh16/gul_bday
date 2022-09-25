7f281ec01cf5465f930f8b0b3f0879b95ff8c618
Jimmy <tarunpreetsingh16@gmail.com>
2022-09-25 00:23:40 -0400

    Updates

---
+++ precache-manifest.1ed5a6d526e9a178949551050ab34f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7abaa844c48781de9f0",
+    "url": "/gul_bday/static/css/main.a395da18.chunk.css"
+  },
+  {
+    "revision": "f7abaa844c48781de9f0",
+    "url": "/gul_bday/static/js/main.f7abaa84.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/gul_bday/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "2acd0e2af9dfab1a648d",
+    "url": "/gul_bday/static/js/2.2acd0e2a.chunk.js"
+  },
+  {
+    "revision": "6c3af5c55cff16111318",
+    "url": "/gul_bday/static/js/runtime~main.6c3af5c5.js"
+  },
+  {
+    "revision": "8a60333ac1b90c49b8b6bd05e5ce68b1",
+    "url": "/gul_bday/index.html"
+  }
+];
