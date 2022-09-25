1d4a9305fed1332cfb0be326e3aec6b3c6c38e75
Jimmy <tarunpreetsingh16@gmail.com>
2022-09-25 00:16:52 -0400

    Updates

---
+++ precache-manifest.19be003ae3306160332775f3ccd06dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "083fed9d4716018eedd3",
+    "url": "/gul_bday/static/css/main.857115b1.chunk.css"
+  },
+  {
+    "revision": "083fed9d4716018eedd3",
+    "url": "/gul_bday/static/js/main.083fed9d.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/gul_bday/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/gul_bday/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "6c3af5c55cff16111318",
+    "url": "/gul_bday/static/js/runtime~main.6c3af5c5.js"
+  },
+  {
+    "revision": "8db7c21885a3c0d27416588f51c9b644",
+    "url": "/gul_bday/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gul_bday/precache-manifest.25bd254c1accab2b3046babe1d0ed547.js"
+  "/gul_bday/precache-manifest.19be003ae3306160332775f3ccd06dd7.js"
 );
 
 workbox.clientsClaim();
